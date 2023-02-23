18096e2f685b8a316d3f7967c8b73cb70ff8b891
AlexiaNomena <missralexia1@gmail.com>
2023-02-23 12:33:33 +0100

    updated

--- Methods/figure_settings.py
+++ Methods/figure_settings.py
@@ -47,23 +47,33 @@ def Display(t, Y, is_log, labels, figsize = (7, 7), xysize = (15,15), labsize = 
 import seaborn as sns
 
 
-def Heatmap(data_dic, row_labels, col_labels, annotsize = 12 ,colormap = None, save_to = "cross_reactivity", sub_fig_size = 6):
+def Heatmap(data_dic, row_labels, col_labels, annotsize = 12 ,colormap = None, save_to = "cross_reactivity", sub_fig_size = 6, num_row_col = None):
     dLabs = list(data_dic.keys())
     N = len(dLabs)
-    if N%2 == 0:
-        F = int(N/2) 
-    else:
-        F = (N//2) + 1
+    
     PreFig()
-    fig = plt.figure(figsize = (F*sub_fig_size, F*sub_fig_size))
+    if num_row_col is None:
+        if N%2 == 0:
+            F = int(N/2) 
+        else:
+            F = (N//2) + 1
+        fig = plt.figure(figsize = (F*sub_fig_size, F*sub_fig_size))
+    else:
+        fig = plt.figure(figsize = (num_row_col[1]*sub_fig_size, num_row_col[0]*sub_fig_size))
+    
     num = 1
     for i in range(N):
-        ax = fig.add_subplot(F, F, num) 
+        if num_row_col is None:
+            ax = fig.add_subplot(F, F, num)
+        else:
+            ax = fig.add_subplot(num_row_col[0], num_row_col[1], num)
+        
         cMap = sns.heatmap(data = data_dic[dLabs[i]], cmap = colormap, xticklabels = row_labels, yticklabels = col_labels, 
                        cbar = False, annot = True, fmt = ".2f", annot_kws = {"size":annotsize})
         num +=1
-        plt.title("FR to Ab "+dLabs[i], fontsize = 16)
-    
+        plt.title("FR to AB "+dLabs[i], fontsize = 16)
+        plt.xticks(rotation = 90)
+
     #plt.subplots_adjust(top=0.965, bottom=0.095, left=0.080, right = 0.75, hspace=0.175, wspace=0.175)
     plt.subplots_adjust(hspace=0.4, wspace=0.4)    
     pdf = PdfPages(save_to + ".pdf")
